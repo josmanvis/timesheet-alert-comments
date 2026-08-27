4ea64dfa590f4195ae9680d881da94f9fbcd02e7
admin <admin@Retina.lan>
2026-08-27 18:30:07 -0400

    Update Cask version to 1.0.1

--- Casks/timesheet-alert-comments.rb
+++ Casks/timesheet-alert-comments.rb
@@ -1,5 +1,5 @@
 cask "timesheet-alert-comments" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/josmanvis/timesheet-alert-comments/releases/latest/download/TimesheetAlertComments.dmg"
