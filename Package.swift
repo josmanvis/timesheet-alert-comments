7f7ba0b5ccd2ece41db3ccbf3fe85075a88bb27a
admin <admin@Retina.lan>
2026-08-27 17:45:31 -0400

    Fix macOS version requirement to support 12.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "timesheet-alert-comments",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v12)
     ],
     products: [
         .executable(name: "timesheet-alert-comments", targets: ["timesheet-alert-comments"]),
